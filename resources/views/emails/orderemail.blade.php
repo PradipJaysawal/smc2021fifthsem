50d1eb8279a74292035dce7f5ddc4dcec44ea891
PradipJaysawal <gabbarsamba948@gmail.com>
2024-09-19 21:50:10 +0545

    mail

--- resources/views/emails/orderemail.blade.php
+++ resources/views/emails/orderemail.blade.php
@@ -8,5 +8,5 @@
         Your Package: <br>{{$package}} <br> is Now {{$status}}. <br>
         Thank you for shopping with us. <br>
     </p>
-    
+    <a href="{{route('home')}}" class="px-2 py-1 bg-green-600 rounded-lg text-white">Click here</a>
 </div>
